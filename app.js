02c05eec9ef41ff5765ad28f48fbbe1b684fd173
Connie <constancecrawford87@gmail.com>
2021-10-20 09:11:19 -0400

    functions

---
+++ app.js
@@ -0,0 +1,50 @@
+function myAdd(argument1, argument2){
+    console.log(myAdd(1,2));
+}
+function myOtherFunction(argument1, argument2, argument3){
+    console.log(myOtherFunction(1,2,3));
+}
+function myLastFunction(argument1, argument2, argument3, argument4){
+    console.log(myLastFunction(1,2,3,4));
+}
+
+var breakfast = {
+    food: "Pancakes",
+    Drink: "Orange Juice",
+    Time: "Morning",
+};
+breakfast.Cereal = "Frosted Flakes"
+myBreakfast["food"];
+myBreakfast["Drink"];
+myBreakfast["Time"];
+
+console.log(breakfast.food)
+console.log(['pancakes']);
+
+var lunch = {
+    food: "Pizza",
+    Drink: "Water",
+    Time: "Afternoon",
+};
+lunch.place = "Restuarant";
+
+myLunch["food"];
+myLunch["Drink"];
+myLunch["Time"];
+
+console.log(lunch.food)
+console.log(['pizza']);
+
+var dinner = {
+    food: "Pasta",
+    Drink: "Wine",
+    Time: "Evening",
+};
+dinner.taste = "Bitter";
+
+myDinner["food"];
+myDinner["Drink"];
+myDinner["Time"];
+
+console.log(dinner.food)
+console.log(['pasta']);
